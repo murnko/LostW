a2c1f075f169f2199b152023cc7030b162b8e43a
murnko <dawid.kamrowski@gmail.com>
2015-08-29 19:22:56 +0200

    dodany przemienny ruch HV, działa dla deterministycznych światów

--- agents/new_agent.py
+++ agents/new_agent.py
@@ -23,6 +23,7 @@ class Agent:
         self.map = areaMap  # mapa wlasciwa
         self.belief = []
         self.horizontal_move = 0
+        self.horizontal_done = 0
         # w tym przykladzie histogram wypelniany rownymi wartosciami
         sum_norm = self.height * self.width
         self.hist = []
@@ -49,9 +50,6 @@ class Agent:
                     self.exit.append(y)
                     self.exit.append(x)
                 self.map_num[y].append(pole)
-
-
-
         return
 
     # nie zmieniac naglowka metody, tutaj agent dokonuje obserwacji swiata
@@ -69,8 +67,8 @@ class Agent:
                     if self.map_num[y][x] == 0:
                         self.hist_z[y][x] += 1
 
-        print self.hist_z
-        print self.map_num
+        #  print self.hist_z
+        #  print self.map_num
 
         pass
 
@@ -79,22 +77,29 @@ class Agent:
     def move(self):
         # ustalenie polozenia od najwyzszego prawdopodobienstwa, pierwszego znalezionego
         max_prob = 0
-        self.hist_z[9][4] = 3
+        # self.hist_z[4][3] = 3
         for y in range(self.height):
             for x in range(self.width):
                 if self.hist_z[y][x] > max_prob:
                     max_prob = self.hist_z[y][x]
                     self.belief = [y, x]
             #  awaryjny uniform tu bedzie
+
+
+        #  print self.belief
+
+        if self.horizontal_done:
+            self.horizontal_move = 0
+        else:
+            self.horizontal_move = 1
+
         if self.belief[0] == self.exit[0]:
             self.horizontal_move = 1
         elif self.belief[1] == self.exit[1]:
             self.horizontal_move = 0
 
-        print self.belief
-        print self.exit
-
         if self.horizontal_move:
+            self.horizontal_done = 1
             if abs(self.belief[1] - self.exit[1]) > self.width/2:
                 if (self.belief[1] - self.exit[1]) > 0:
                     move = 'r'
@@ -106,6 +111,7 @@ class Agent:
                 else:
                     move = 'r'
         else:
+            self.horizontal_done = 0
             if abs(self.belief[0] - self.exit[0]) > self.height/2:
                 if (self.belief[0] - self.exit[0]) > 0:
                     move = 'd'
@@ -116,24 +122,29 @@ class Agent:
                     move = 'u'
                 else:
                     move = 'd'
-
-        print move
-        print self.horizontal_move
-        sys.exit("czy histogram1?")
+        #  print move
         #  musi nastapic przesuniecie histogramu
-        #  self.histogram = array(self.hist_z) <-- na potrzeby szybkiego rolla
+        #  print self.hist_z
+        #  print self.hist_z
         if move == 'r':
-            roll(self.histogram, 1, axis=1)
+            self.hist_z = roll(self.hist_z, 1, axis=1)
+            return Action.RIGHT
         elif move == 'l':
-            roll(self.histogram, -1, axis=1)
+            self.hist_z = roll(self.hist_z, -1, axis=1)
+            return Action.LEFT
         elif move == 'u':
-            roll(self.histogram, -1, axis=0)
+            self.hist_z = roll(self.hist_z, -1, axis=0)
+            return Action.UP
         elif move == 'd':
-            roll(self.histogram, 1, axis=0)
+            self.hist_z = roll(self.hist_z, 1, axis=0)
+            return Action.DOWN
+
+        sys.exit("czy histogram1?")
 
     # nie zmieniac naglowka metody, tutaj agent udostepnia swoj histogram (ten z filtru
     # histogramowego), musi to byc tablica (lista list, krotka krotek...) o wymarach takich jak
     # plansza, pobranie wartosci agent.histogram()[y][x] zwraca prawdopodobienstwo stania na polu
     # w wierszu y i kolumnie x
+
     def histogram(self):
         return self.hist
